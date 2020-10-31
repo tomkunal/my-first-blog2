6275e2ae32ebb014c55638b6381986dcbe747134
tomkunal <73659893+tomkunal@users.noreply.github.com>
2020-11-01 00:02:23 +0530

    Update settings.py

--- devtestDBcheck/settings.py
+++ devtestDBcheck/settings.py
@@ -139,6 +139,12 @@ DATABASES = {
 }
 
 """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  'db.sqlite3',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
